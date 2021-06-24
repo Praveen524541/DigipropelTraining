1198f98faf2cf9cb22c4baad63e8d38a01485ccd
Praveen524541 <86397845+Praveen524541@users.noreply.github.com>
2021-06-24 19:36:20 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace c__1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int i,n; 
+           float a=0;
+
+
+    int v=int.Parse(Console.ReadLine());
+	for (i=1;i<=v;i++) 
+	{ 
+       Console.Write("Number-{0} :",i); 
+       n= int.Parse(Console.ReadLine());	
+        a=a+n;
+        	 
+	} 
+    float z=a/v;
+    Console.WriteLine(z);
+        }
+    }
+}
+
